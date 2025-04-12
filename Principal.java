b484ef521ebd5520fe08051a6bf26c7cfc23033d
Tomas Palermo <109637082+Palermukii@users.noreply.github.com>
2025-04-11 22:23:07 -0300

    Actualizacion del codigo
    - Faltaria agregar el Modo prueba
    - Faltaria agergar la funcion de calcular estadisticas junto con su opcion en el menu
    - Faltaria arreglar el tema de los arrays null
    (FUNCIONA PERO SOLO PORQUE SE OMITE MOSTRAR LOS QUE SON NULL)

--- Principal.java
+++ Principal.java
@@ -19,7 +19,7 @@ public class Principal{
 		Scanner s = new Scanner(System.in);
 		int cantEquipos = 0;
 		
-		int opc = 0;
+		int opc;
 		do {
 			opc = mostrarMenuYElegirOpcion(s);
 			cantEquipos = generarAccion(s,opc,equipos, cantEquipos, ID_MIN, ID_MAX, VIDEOJUEGOS,MAX_JUEGOS, MIN_JUEGOS, MAX_JUGADORES, MIN_JUGADORES);
@@ -28,7 +28,7 @@ public class Principal{
 
 	public static int ingresarEquipo(Scanner s, final String[][] EQUIPOS, int cantEquipos, final int ID_MIN, final int ID_MAX, final String[] VIDEOJUEGOS, final int MAX_JUEGOS, final int MIN_JUEGOS) {
 		// ID
-		int indiceIdBuscado = -1;
+		int indiceIdBuscado;
 		int idEquipo = 0;
 		do {
 			System.out.println("Ingrese el ID del equipo: ");
@@ -41,7 +41,7 @@ public class Principal{
 		}while(indiceIdBuscado >= 0);
 
 		// NOMBRE
-		int indiceNombreBuscado = -1;
+		int indiceNombreBuscado;
 		String nombreEquipo;
 		do {
 			System.out.println("Ingrese el nombre del equipo");
@@ -61,6 +61,7 @@ public class Principal{
 		for(int i = 0; i<VIDEOJUEGOS.length; i++){
 			System.out.println(i+1+". "+VIDEOJUEGOS[i]);
 		}
+		System.out.println("\n");
 
 		final int VIDEOJUEGO=ingresarEntero(MAX_JUEGOS, MIN_JUEGOS, s);
 	
@@ -116,6 +117,7 @@ public class Principal{
 		System.out.println("9)  Ver tabla de posiciones\n");
 		System.out.println("10) Calcular estadísticas del torneo\n");
 		System.out.println("11) Salir\n");
+		imprimirGuiones();
 		return ingresarEntero(11,1,s);
 	}
 
@@ -132,12 +134,20 @@ public class Principal{
 			case 3:
 				modificarEquipo(s,EQUIPOS,cantEquipos,ID_MAX, ID_MIN, MAX_JUEGOS, MIN_JUEGOS, VIDEOJUEGOS, MAX_JUGADORES, MIN_JUGADORES);
 			break;
+			case 4:
+				eliminarEquipo(s, EQUIPOS, cantEquipos, ID_MIN, ID_MAX);
+			break;
 			case 5: // CONSULTAR TODOS LOS EQUIPOS
 				System.out.println("Los equipos registrados son: ");
 				for(int i = 0; i < cantEquipos; i++) {
-					mostrarDatoEquipo(EQUIPOS, i, VIDEOJUEGOS);
-					System.out.println("-------------------------------------------");
-				}
+					if(EQUIPOS[i] != null) {
+						mostrarDatoEquipo(EQUIPOS, i, VIDEOJUEGOS);
+						imprimirGuiones();
+					}
+					}
+			break;
+			case 6:
+				registrarPartido(EQUIPOS, cantEquipos, s);
 			break;
 			case 7:
 			buscarPorVideojuego(s, EQUIPOS, VIDEOJUEGOS, MAX_JUEGOS, 1, cantEquipos);
@@ -153,6 +163,9 @@ public class Principal{
 	public static int buscarEquipo(final String[][] EQUIPOS, final int BUSCADO, final int LONGITUD) {
 		int i = 0;
 		while(i<LONGITUD) {
+			if(EQUIPOS[i] == null) {
+				return -1;
+			}
 			if(Integer.parseInt(EQUIPOS[i][0]) == BUSCADO) { //indice 0 = id. Busca por id
 				return i;
 			}
@@ -178,7 +191,7 @@ public static void consultarEquipo(Scanner s, final String[][] EQUIPOS, final in
 		CONSULTADO = ingresarEntero(ID_MAX, ID_MIN, s);
 		INDICE=buscarEquipo(EQUIPOS, CONSULTADO, N_EQUIPOS_MAX);
 
-		if(INDICE == -1) {
+		if(INDICE == -1 || EQUIPOS[INDICE][0] == null) {
 			System.out.println("Ese equipo no existe");
 			return;
 		}
@@ -188,6 +201,7 @@ public static void consultarEquipo(Scanner s, final String[][] EQUIPOS, final in
 
 	}
 	public static void mostrarDatoEquipo(final String[][] EQUIPOS, final int INDICE, final String[] VIDEOJUEGOS) {
+		
 		System.out.println("ID. " + EQUIPOS[INDICE][0]);
 		System.out.println("NOMBRE. "+ EQUIPOS[INDICE][1]);
 		System.out.println("VIDEOJUEGO. "+ VIDEOJUEGOS[Integer.parseInt(EQUIPOS[INDICE][2]) -1]);
@@ -196,8 +210,7 @@ public static void consultarEquipo(Scanner s, final String[][] EQUIPOS, final in
 		System.out.println("PARTIDOS GANADOS. " + EQUIPOS[INDICE][5]);
 		System.out.println("PARTIDOS PERDIDOS. " + EQUIPOS[INDICE][6]);
 		System.out.println("PUNTOS TOTALES. " + EQUIPOS[INDICE][7]);
-		System.out.println("------------------------------------------------------------------------");
-
+		imprimirGuiones();
 	}
 
 	public static String ingresarCadena(Scanner s) {
@@ -259,7 +272,7 @@ public static void consultarEquipo(Scanner s, final String[][] EQUIPOS, final in
 
 	public static int ingresarEntero(final int MAX, final int MIN, Scanner s) {
         int nro =0;
-        boolean error = false;
+        boolean error;
         do{
 			error = false;
             try {
@@ -362,7 +375,7 @@ public static void consultarEquipo(Scanner s, final String[][] EQUIPOS, final in
 				break;
 		}
 		System.out.println("Asi quedaria modificado");
-		System.out.println("------------------------");
+		imprimirGuiones();
 		mostrarDatoEquipo(EQUIPOS, indice, VIDEOJUEGOS);
 
 
@@ -373,8 +386,58 @@ public static void consultarEquipo(Scanner s, final String[][] EQUIPOS, final in
 	}
 	
 
+	public static int eliminarEquipo(Scanner s, final String[][] EQUIPOS, int cantEquipos, final int ID_MIN, final int ID_MAX) {
+		System.out.println("Ingrese el id del equipo a eliminar: ");
+		int idBuscado = ingresarEntero(ID_MAX,ID_MIN,s);
+		final int idEquipo = buscarEquipo(EQUIPOS, idBuscado, cantEquipos);
+		if(idEquipo == -1) {
+			System.out.println("No se encontró el ID del equipo");
+		} else {
+			for (int i = idEquipo; i < cantEquipos-1; i++) {
+				EQUIPOS[i] = EQUIPOS[i+1];
+			}
+			EQUIPOS[cantEquipos-1] = null; // Limpiar la ultima posicion
+			System.out.println("Equipo eliminado exitosamente");
+			cantEquipos--;
+		}
+		
+		return cantEquipos;
+	}
 
+	public static void registrarPartido(final String[][] EQUIPOS, final int CANT_EQUIPOS, Scanner s) {
+		System.out.println("Ingrese el id del equipo que ganó el partido: ");
+		int idGanador = ingresarEntero(8000, 10, s);
+		int indiceGanador = buscarEquipo(EQUIPOS, idGanador, CANT_EQUIPOS);
+		if(indiceGanador == -1) {
+			System.out.println("No se encontró el ID del equipo ganador");
+			return;
+		}
+		System.out.println("Ingrese el id del equipo que perdió el partido: ");
+		int idPerdedor = ingresarEntero(8000, 10, s);
+		int indicePerdedor = buscarEquipo(EQUIPOS, idPerdedor, CANT_EQUIPOS);
+		if(indicePerdedor == -1) {
+			System.out.println("No se encontró el ID del equipo perdedor");
+			return;
+		}
+		
+		// Comprobar que jueguen al mismo juego
+		if(!EQUIPOS[indiceGanador][2].equals(EQUIPOS[indicePerdedor][2])) {
+			System.out.println("Los equipos no juegan al mismo videojuego. No se puede registrar el partido.");
+			return;
+		}
 
+		int partidosGanados = Integer.parseInt(EQUIPOS[indiceGanador][5]);
+		int partidosPerdidos = Integer.parseInt(EQUIPOS[indicePerdedor][6]);
+		
+		EQUIPOS[indiceGanador][5] = String.valueOf(partidosGanados + 1);
+		EQUIPOS[indicePerdedor][6] = String.valueOf(partidosPerdidos + 1);
+		int puntosGanador = Integer.parseInt(EQUIPOS[indiceGanador][7]);
+		EQUIPOS[indiceGanador][7] = String.valueOf(puntosGanador + 3);
+		System.out.println("Partido registrado exitosamente");
+	}
 
+static void imprimirGuiones(){
+	System.out.println("------------------------------------------------------------------------");
+}
 
 }
